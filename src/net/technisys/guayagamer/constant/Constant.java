12e011e1076474a391d33ede3dacfa15c37953f3
santiago <santiago@correo.com>
2020-02-27 23:59:54 -0500

    incremental

---
+++ src/net/technisys/guayagamer/constant/Constant.java
@@ -0,0 +1,22 @@
+package net.technisys.guayagamer.constant;
+
+public class Constant {
+
+	public static final int INITIAL_HOUR = 9;
+	public static final int LUNCH_HOUR = 12;
+	public static final int MIN_FINAL_HOUR = 16;
+	public static final int MAX_FINAL_HOUR = 17;
+	
+	public static final int MIN_HOUR_PER_ROOM = 6;
+	public static final int MAX_HOUR_PER_ROOM = 7;
+	
+	public static final int MIN_HOURS_SESSION1 = 3;
+	public static final int MAX_HOURS_SESSION1 = 3;
+	
+	public static final int MIN_HOURS_SESSION2 = 3;
+	public static final int MAX_HOURS_SESSION2 = 4;
+	
+	
+	
+	
+}
